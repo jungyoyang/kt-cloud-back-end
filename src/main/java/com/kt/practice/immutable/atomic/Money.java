8610d9614c6896b4b033041b5de04957cb0fb679
Jungyo Yang(Window) <2jungyo@naver.com>
2025-10-26 22:18:44 +0900

    stream API공부중 10/26 22:18

--- src/main/java/com/kt/practice/immutable/atomic/Money.java
+++ src/main/java/com/kt/practice/immutable/atomic/Money.java
@@ -1,9 +1,18 @@
 package com.kt.practice.immutable.atomic;
 
+// Java에서 “불변 클래스(Immutable Class)”란
+// **‘객체가 한 번 만들어진 이후에는 상태가 절대 변하지 않는 클래스’**를 의미합니다.
+//이때 “상태”란 그 클래스가 가지고 있는 모든 인스턴스 변수들을 의미합니다.
+// 즉, 단 하나라도 바뀔 수 있는 필드가 있으면 불변성이 깨져요.
+// '불변성’은 클래스 이름에 붙는 게 아니라 객체의 동작 방식이에요
+// 즉, 클래스 선언에 final을 붙이는 게 아니라,
+// 그 안의 “동작 방식”으로 불변을 보장하는 거예요.
+
 public class Money {
 	final int amount;
 
 	public Money(int amount) {
+		//객체의 고정값을 넣는 순간
 		this.amount = amount;
 	}
 
--- src/main/java/com/kt/practice/stream/StreamMain.java
+++ src/main/java/com/kt/practice/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kt.practice.stream;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 	public static void main(String[] args) {
@@ -24,16 +25,20 @@ public class StreamMain {
 		 * ex) 객체.stream생성().중간연산.최종연산()
 		 */
 
-		List<String> names = List.of("Alice", "Bob", "Charlie");
+		List<String> names = List.of("Alice", "Bob", "Charlie", "David", "Eve");
 		names.add("Frank"); //UnsupportedOperationException, of는 immutable이기 때문ㅇ
 
+		//names를 복사해서 새로운 것 생성
 		//알파벳 e가 들어간 이름들만 모아서 출력 (filter)
-		names.stream().filter().
+		var newNames = names.stream()
+			.filter(name -> name.contains("e"))
+			.collect(Collectors.toList());
 
-			//names를 복사해서 새로운 것 생성
-			var newNames = names.stream().filter(name -> name.contains("e"))
-
+		System.out.println(newNames);
 		//jdk8부터 stream
-		//jdk11부터
+		//jdk11부터 Collectors.toList는 너무많이쓰더라, 그래서 그냥 toList();로 사용가능
+		/*var newNames = names.stream()
+			.filter(name -> name.contains("e"))
+			.toList();*/
 	}
 }
